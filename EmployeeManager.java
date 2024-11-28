728b925b6ae0d6d0ab4a01ad0ada8174b5270d3c
mdrion12 <reon128633@gmail.com>
2024-11-28 10:13:20 +0600

    add a check at the begining to the correct argument

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -5,6 +5,11 @@ import java.util.*;
 
 public class EmployeeManager {
     public static void main(String[] args) {
+
+        if (args.length > 1) {
+            System.out.println("No argument provide");
+            return;
+        }
         // Check arguments
         if (args[0].equals("l")) {
             System.out.println("Loading data ...");
